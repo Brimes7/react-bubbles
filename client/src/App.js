3dd368d036e48cc55ed4898a9012eb560c6fbbd1
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2019-10-14 22:55:25 -0500

    Updated Login Page still has bugs

--- client/src/App.js
+++ client/src/App.js
@@ -10,13 +10,39 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+    
+          {/* Build a PrivateRoute component that will 
+          display BubblePage when you're authenticated  */}
+      
+        <ul>
+          <li>
+            <Link to="/public">Public Page</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Route path="/public" component={Public} />
+        <Route path="/login" component={Login} />
+        <PrivateRoute path='/protected' component={Protected} />
+  
       </div>
     </Router>
   );
 }
+
+//This passes in all the props, checks if the User is authenticated, renders component, and if its not logged in it directs you back to login page.
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
 
 export default App;
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import { axiosWithAuth } from "../../../util/axiosAuth";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
-  axios.post('', userCredintials)
+  axios.post('', userCredentials)
     .then(res => {
       localStorage.setItem('token', res.data.token);
       props.history.push('/dashboard');
@@ -12,14 +12,50 @@ const Login = () => {
     // axiosWithAuth().get('endpoint/path/here').then(data => /* do something with the data */);
 
     //need to figure out how to use this
+  const [credentials, setCredentials] = useState({});
   
+
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth().post('login/App', credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.token);
+        this.props.history.push('/');
+      });
+  }
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value})
+      }
+  }
+
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    )
+
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <h1>Welcome to the Bubble App!</h1>
+//       <p>Build a login page here</p>
+//     </>
+//   );
+// };
 
 export default Login;
