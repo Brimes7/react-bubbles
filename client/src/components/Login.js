1e4edabe36cb0334b7f25982b49ea53212da8dd7
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2019-10-14 22:02:37 -0500

    Updated and added axiosAuth and created Token.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { axiosWithAuth } from "../../../util/axiosAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
+  axios.post('', userCredintials)
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      props.history.push('/dashboard');
+    })
+  
+    // axiosWithAuth().get('endpoint/path/here').then(data => /* do something with the data */);
+
+    //need to figure out how to use this
+  
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
